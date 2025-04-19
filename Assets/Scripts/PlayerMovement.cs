21b16fa631f4327844cd749b215be54f90320204
Le Tri <trile20021511@gmail.com>
2025-04-19 09:16:51 +0700

    feat: input setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        moveInput = inputValue.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
