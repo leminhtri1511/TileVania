f76fa0bf4040ee63f5db2672eb201f82baea3c72
Le Tri <trile20021511@gmail.com>
2025-04-19 12:17:49 +0700

    feat: flip player while moving left right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,21 +8,40 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+
+    [SerializeField] float runSpeed = 2f;
 
     void Start()
     {
-
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
-
 
     void Update()
     {
+        Run();
+        FlipSprite();
+    }
 
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
     }
 
     void OnMove(InputValue inputValue)
     {
         moveInput = inputValue.Get<Vector2>();
-        Debug.Log(moveInput);
+        // Debug.Log($"X: {myRigidbody.velocity.x}");
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > 0;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
     }
 }
